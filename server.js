ff9dd7f6142ea6353d861db64f9bf0c3acdd0f7b
tratchapong <tratchapong@gmail.com>
2024-06-19 14:21:04 +0700

    login ok

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const app = express()
 app.use('/register', (req,res)=>{
 	res.json({msg: 'Register...'})
 })
+app.use('/login', (req, res) => {
+	res.json({msg: 'Login...'})
+})
 
 app.use((req,res)=>res.status(404).json({msg:'path not found'}))
+
 
 const port = 8000
 app.listen(port, ()=>console.log('Server on',port))
